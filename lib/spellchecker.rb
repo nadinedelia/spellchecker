d8ca7c01062f3de57cf2a2ca091432c0afdb5cf3
nadinedelia <nadine.delia@yahoo.com>
2020-08-10 14:27:14 +0200

    failing test map function

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -2,16 +2,25 @@
 class SpellChecker
 
     
+    attr_reader :dictionary
+
+
     def initialize
-        @dictionary = ['hello']
+        @dictionary = ['hello', 'world', 'there', 'are', 'sun']
     end
 
     def check(string)
-        if @dictionary.include?(string)
-            return string
+
+        words_array = string.split(' ')
+
+        changed_array = words_array.map do |word|
+        if dictionary.include?(word)
+            return word
         else
-            return "~#{string}~"
+            return "~#{word}~"
         end
+    end
+    return changed_array.join(' ')
     end
 
 
--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -6,4 +6,16 @@ describe SpellChecker do
         expect(subject.check('hello')).to eq 'hello'
     end
 
+    it 'checks if word is not part of word bank and if so, returns it changed' do
+        expect(subject.check('hlleo')).to eq '~hlleo~'
+    end
+
+    it 'returns a string consisting of more than 1 word correctly if spelled right' do
+        expect(subject.check('hello world')).to eq 'hello world'
+    end
+
+    it 'returns a changed string consisting of more than 1 word correctly if spelled wrong' do
+        expect(subject.check('hlleo world')).to eq '~hlleo world~'
+    end
+
 end
