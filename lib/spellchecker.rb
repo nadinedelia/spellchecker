3776ff076d8024c450a97ff3cc1fc391da623803
nadinedelia <nadine.delia@yahoo.com>
2020-08-10 14:33:54 +0200

    refactor

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -6,21 +6,20 @@ class SpellChecker
 
 
     def initialize
-        @dictionary = ['hello', 'world', 'there', 'are', 'sun']
+        @dictionary = ['hello', 'world', 'there', 'are', 'sun', 'is']
     end
 
     def check(string)
 
         words_array = string.split(' ')
 
-        changed_array = words_array.map do |word|
+        changed_array = words_array.map { |word|
         if dictionary.include?(word)
             word = word
         else
             word = "~#{word}~"
         end
-        end
-
+    }
         return changed_array.join(' ')
      
     end
--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -1,13 +1,12 @@
 require 'spellchecker'
 
 describe SpellChecker do
-    
+
+
+    context 'string is returned unchanged' do
+        
     it 'checks if word is part of word bank and if so, returns it unchanged' do
         expect(subject.check('hello')).to eq 'hello'
-    end
-
-    it 'checks if word is not part of word bank and if so, returns it changed' do
-        expect(subject.check('hlleo')).to eq '~hlleo~'
     end
 
     it 'returns a string consisting of more than 1 word correctly if spelled right' do
@@ -15,7 +14,26 @@ describe SpellChecker do
     end
 
     it 'returns a changed string consisting of more than 1 word correctly if spelled wrong' do
+        expect(subject.check('hello world there is sun')).to eq 'hello world there is sun'
+    end
+
+end
+
+
+    context 'string is returned changed' do
+    it 'checks if word is not part of word bank and if so, returns it changed' do
+        expect(subject.check('hlleo')).to eq '~hlleo~'
+    end
+
+    it 'returns a changed string consisting of more than 1 word correctly if spelled wrong' do
         expect(subject.check('hlleo world')).to eq '~hlleo~ world'
     end
+
+
+    it 'returns a changed string consisting of more than 1 word correctly if spelled wrong' do
+        expect(subject.check('hello world thre is sun')).to eq 'hello world ~thre~ is sun'
+    end
+
+end
 
 end
